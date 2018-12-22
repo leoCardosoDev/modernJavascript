9e5618618978c22e1be6c168caf1325d7b469d97
leoCardosoDev <leocardosodev@gmail.com>
2018-12-22 15:22:58 -0200

    Começando os projetos em javascript

---
+++ 1-Dom-Project/tasklist/app.js
@@ -0,0 +1,42 @@
+// Define UI Vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners(){
+    // Add task event
+}
+
+
+
+// Add Task
+
+  // Create li element
+
+  // Add class
+
+  // Create text node and append to li
+
+  // Create new link element
+
+  // Add class
+
+  // Add icon html
+
+  // Append the link to li
+
+
+  // Append li to ul
+
+
+  // Clear input
+
+
+
