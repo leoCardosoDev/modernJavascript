0d8c0e336f8c63edf901f393030556612ef9947e
leoCardosoDev <leocardosodev@gmail.com>
2018-12-28 14:58:35 -0200

    Console

---
+++ 00-Fundamentals/01-Console/app.js
@@ -0,0 +1,54 @@
+// Line Comment
+
+/*
+    Multi 
+    lines 
+    Comments
+ */
+
+//Log console
+console.log('Hello World!');
+console.log(123);
+console.log(true);
+
+var greeting = "Hello World";
+console.log(greeting);
+console.log(1, 2, 3, 4, 5);
+console.log({a:1, b:2});
+console.log(document)
+console.log(window)
+
+//Table Console
+const jsonO = {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+}
+const objeto = {
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false
+}
+console.table(jsonO);
+console.table(objeto);
+
+
+// Error Console
+console.error('Erro de console');
+// Warning Console
+console.warn('Mensagem de Alerta!');
+
+// Clear Console
+console.clear();
+
+//Time Console
+console.time('Hello');
+    console.table(jsonO);
+    console.table(objeto);
+    console.log('Hello World!');
+    console.log('Hello World!');
+    console.log('Hello World!');
+console.timeEnd('Hello');
+
