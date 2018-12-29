c2477d98f970adc780607250af78033fb9cee0da
leoCardosoDev <leocardosodev@gmail.com>
2018-12-29 12:24:30 -0200

    Data Types

---
+++ 00-Fundamentals/03-Data-Types/app.js
@@ -0,0 +1,21 @@
+/*
+    PRIMITIVE DATA TYPES
+    1. String
+    2. Number
+    3. Boolean
+    4. Null
+    5. Undefined
+    6. Symbols (ES6)
+
+    REFERENCE DATA TYPES
+    1. Arrays
+    2. Object Literal
+    4. Functions
+    4. Dates
+    5. Anything Else
+*/
+console.clear();
+
+//STRING
+const name = 'Leo Cardoso';
+console.log(name, ' = ', typeof name);
