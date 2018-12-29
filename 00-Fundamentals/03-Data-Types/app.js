a070b44f34da3761918f9d4a7c2a1916462a86d1
leoCardosoDev <leocardosodev@gmail.com>
2018-12-29 12:53:24 -0200

    Data Types

--- 00-Fundamentals/03-Data-Types/app.js
+++ 00-Fundamentals/03-Data-Types/app.js
@@ -10,12 +10,60 @@
     REFERENCE DATA TYPES
     1. Arrays
     2. Object Literal
-    4. Functions
+    3. Functions
     4. Dates
     5. Anything Else
 */
 console.clear();
 
-//STRING
+/***    PRIMITIVE DATA TYPES    ***/
+// 1. STRING
 const name = 'Leo Cardoso';
 console.log(name, ' = ', typeof name);
+
+// 2. NUMBER
+const age = 36;
+console.log(age, ' = ', typeof age );
+
+// 3. BOLLEAN
+const hasKid = true;
+console.log(hasKid, ' = ', typeof hasKid);
+
+// 4. NULL
+const car = null;
+console.log(car, ' = ', typeof car );
+
+// 5. UNDEFINED
+let wife;
+console.log(wife, ' = ', typeof wife );
+let house = undefined;
+console.log(house, ' = ', typeof house);
+
+// 6. SYMBOL
+const sym = Symbol();
+console.log(sym, ' = ', typeof sym);
+
+/***    REFERENCE DATA TYPES - OBJECT   ***/
+// 1. ARRAY
+const hobbies = ['movies', 'music'];
+console.table(hobbies);
+console.log(' = ', typeof hobbies);
+
+// 2. OBJECT LITERAL
+const address = {
+    city: 'Barueri',
+    state: 'SP'
+};
+console.table(address);
+console.log(' = ',typeof address);
+
+// 3. FUNCTIONS
+const task = function(){
+    return "Caminhar"
+}
+console.log(typeof task);
+
+// 4. DATE
+const today = new Date();
+console.log(today);
+console.log(typeof today);
