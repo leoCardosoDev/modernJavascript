62f4d416b9451a88d1dc02c027790ffa912b86cd
leoCardosoDev <leocardosodev@gmail.com>
2019-01-17 23:05:55 -0200

    Keybord Event

--- 01-Dom-Manipulation-Event/09-Keyboard-Event/app.js
+++ 01-Dom-Manipulation-Event/09-Keyboard-Event/app.js
@@ -3,12 +3,12 @@ console.clear()
 const form = document.querySelector('form')
 const taskInput = document.getElementById('task')
 const heading = document.querySelector('h5')
+const select = document.querySelector('select')
 
 // Clear Input
 taskInput.value = '';
 
 form.addEventListener('submit', runSubmit)
-/*
 // Keydown
 taskInput.addEventListener('keydown', runEvent)
 // Key Up
@@ -23,10 +23,10 @@ taskInput.addEventListener('blur', runEvent)
 taskInput.addEventListener('cut', runEvent)
 // Paste
 taskInput.addEventListener('paste', runEvent)
-*/
 // Input
 taskInput.addEventListener('input', runEvent)
-
+// Change
+select.addEventListener('change', runEvent)
 
 function runEvent(e){
     console.log(`EVENT TYPE: ${e.type}`)
