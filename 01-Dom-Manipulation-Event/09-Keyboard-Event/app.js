7758e4f53df88e6f20519fa69dea594eaf4fe8ad
leoCardosoDev <leocardosodev@gmail.com>
2019-01-17 22:56:25 -0200

    Keyboard Event

---
+++ 01-Dom-Manipulation-Event/09-Keyboard-Event/app.js
@@ -0,0 +1,43 @@
+console.clear()
+/**  EVENT LISTENERS **/
+const form = document.querySelector('form')
+const taskInput = document.getElementById('task')
+const heading = document.querySelector('h5')
+
+// Clear Input
+taskInput.value = '';
+
+form.addEventListener('submit', runSubmit)
+/*
+// Keydown
+taskInput.addEventListener('keydown', runEvent)
+// Key Up
+taskInput.addEventListener('keyup', runEvent)
+// Key Press
+taskInput.addEventListener('keypress', runEvent)
+// Focus
+taskInput.addEventListener('focus', runEvent)
+// Blur
+taskInput.addEventListener('blur', runEvent)
+// Cut
+taskInput.addEventListener('cut', runEvent)
+// Paste
+taskInput.addEventListener('paste', runEvent)
+*/
+// Input
+taskInput.addEventListener('input', runEvent)
+
+
+function runEvent(e){
+    console.log(`EVENT TYPE: ${e.type}`)
+    //console.log(e.target.value)
+    //heading.innerText = e.target.value
+}
+
+function runSubmit(e){
+    console.log(`EVENT TYPE: ${e.type}`)
+    
+    // Get input value
+    console.log(taskInput.value)
+    e.preventDefault()
+}
