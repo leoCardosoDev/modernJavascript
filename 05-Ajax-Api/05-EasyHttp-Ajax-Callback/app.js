f01227e55c8665bdd51691322300c914dd10e9a9
leoCardosoDev <leocardosodev@gmail.com>
2019-01-24 17:34:33 -0200

    EasyHTTP FInal

--- 05-Ajax-Api/05-EasyHttp-Ajax-Callback/app.js
+++ 05-Ajax-Api/05-EasyHttp-Ajax-Callback/app.js
@@ -1,5 +1,6 @@
 const http = new easyHTTP
 
+/* 
 // Get Posts
 http.get('https://jsonplaceholder.typicode.com/posts', (err, posts) => {
 
@@ -9,3 +10,54 @@ http.get('https://jsonplaceholder.typicode.com/posts', (err, posts) => {
         console.log(posts)
     }
 })
+ */
+
+
+
+/* 
+// Get single Post
+http.get('https://jsonplaceholder.typicode.com/posts/1', (err, post) => {
+    if(err){
+        console.log(err)
+    }else{
+        console.log(post)
+    }
+})
+ */
+
+// Create Data
+const data = {
+    title: 'UPDATE - Custom Post 101',
+    body: 'This is a custom post 101'
+}
+
+/* 
+ // Create Post
+ http.post('https://jsonplaceholder.typicode.com/posts', data, (err, post) =>{
+     if(err){
+         console.log(err)
+     }else{
+         console.log(post)
+     }
+ })
+ */
+
+/* 
+// Update Data
+http.put('https://jsonplaceholder.typicode.com/posts/1', data, (err, post) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(post)
+    }
+})
+ */
+
+ // Delet data
+ http.delete('https://jsonplaceholder.typicode.com/posts/100', function(err, response){
+     if(err){
+         console.log(err)
+     }else{
+         console.log(response)
+     }
+ })
